6ba70cd5e3c0ca37d59495ac19cd6f550f07af28
karpetoryba <karpenkokarpenko24@gmail.com>
2024-10-23 11:00:17 +0200

    new commit

---
+++ moyenne/script.js
@@ -0,0 +1,18 @@
+// OPTION 1
+// Tant que l'utilisateur veut ajouter une noxte
+    // On lui demande de saisir une note (prompt)
+    // ajouté la note à la liste de notes (tableau)
+    // On lui demande s'il souhaite ajouter une autre note (confirm)
+
+// OPTION 2
+// Demander à l'utilisateur de saisir une liste de notes séparées par des virgules
+// On transforme la chaine de caractères en tableau avec la méthode split
+
+// affciher la moyenne des notes
+// afficher la note la plus haute
+// afficher la note la plus basse
+
+
+// Bonus
+// afficher la note la plus proche de 10
+// afficher la note la plus proche de 0
---
+++ occurence/occurence-starter.js
@@ -0,0 +1,29 @@
+let p = `
+Faites tremper les pois chiches et les fèves dans l'eau 12 h, les égoutter et les cuire 45 mn à l'auto cuiseur.
+Peler oignon et ail, les hacher ainsi que le persil.
+Passer les fèves et les pois chiches au mixer (ou robot).
+Mélanger avec le persil, l'oignon, l'ail, la farine, les épices, le sel.
+Pétrissez le tout avec vos mains en ajoutant un peu d'eau si nécessaire. Rassemblez la pâte et laisser reposer au réfrigérateur pendant minimum 30 mn.
+Façonner une trentaine de boulettes de la grosseur d'une pièce de 2 euros.
+Les faire frire 2/3 mn puis les égoutter sur du papier absorbant.
+Servir chaud ou froid avec des petites sauces tomates aux herbes, ou sauces yaourts.
+`
+// compter le nombre d'occurence pour chaque mot
+// La casse n'est pas prise en compte
+/********  EXEMPLE */
+/*
+[
+  { word: 'chiches', occurences: 2 },
+  { word: 'les', occurences: 10 },
+  { word: 'pois.', occurences: 2 },
+  { word: 'et', occurences: 5 },
+  ...
+]
+*/
+
+// Bonus : trier les mots par ordre alphabétique
+// Bonus : trier les mots par ordre décroissant du nombre d'occurence
+// Bonus : afficher les 10 mots les plus utilisés
+// Bonus : afficher les 10 mots les moins utilisés
+// Bonus : afficher les 10 mots les plus longs
+// Bonus : afficher les 10 mots les plus courts
