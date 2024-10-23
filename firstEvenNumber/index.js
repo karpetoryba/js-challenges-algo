e0c270a7e6c41b575b5372005ee8eb59d4e831fa
karpetoryba <karpenkokarpenko24@gmail.com>
2024-10-23 12:02:39 +0200

    made 2 assignment

--- firstEvenNumber/index.js
+++ firstEvenNumber/index.js
@@ -1,5 +1,6 @@
 // Trouver le premier nombre pair dans un tableau
 
-
 const numbers = [1, 3, 7, 8, 10];
-findFirstEven(numbers); // 8
+const findFirstEven = numbers.find((element) => element > 7);
+console.log(findFirstEven);
+// 8
