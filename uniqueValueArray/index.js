f4568591d61eefbb238d2d5e56eebe776a484e59
karpetoryba <karpenkokarpenko24@gmail.com>
2024-11-03 20:26:21 +0100

    new function uniqueValueArray

--- uniqueValueArray/index.js
+++ uniqueValueArray/index.js
@@ -1,5 +1,9 @@
-const arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];
-
-
 // Écris une fonction qui prend un tableau en argument et retourne un tableau contenant uniquement des valeurs uniques.
+function uniqueValues(array) {
+  return [...new Set(array)];
+}
+
+const arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+console.log(uniqueValues(arr));
+
 uniqueValues(arr); // [1, 2, 3, 4, 5, 6, 7]
